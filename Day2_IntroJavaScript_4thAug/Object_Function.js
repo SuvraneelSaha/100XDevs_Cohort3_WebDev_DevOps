18ab1341eeaa79eefc9ea8225024740adbad7fa6
Suvraneel _Saha <suvroneel2001@gmail.com>
2024-08-05 00:01:55 +0530

    Commits for 3rd and 4th Aug - Orientation and JavaScript Intro

---
+++ Day2_IntroJavaScript_4thAug/Object_Function.js
@@ -0,0 +1,15 @@
+function greet(user)
+{
+  console.log("Hi "+ user.naam + " your age is "+user.age);
+}
+
+let user = {
+  naam = "kirat",
+  age = 19
+};
+
+greet(user);
+
+
+
+
---
+++ Day2_IntroJavaScript_4thAug/loop1.js
@@ -0,0 +1,15 @@
+let users = ["a", "bb", "ccc", "dddd"];
+
+let totalUsers = users.length;
+
+for (let i = 0; i < totalUsers; i++) {
+  console.log(users[i]);
+}
+
+// initiation section - first happens - open happens once
+
+// condition section -- second
+
+// 3rd - goes into the Body
+
+// 4th - incrementation section -- after the body
